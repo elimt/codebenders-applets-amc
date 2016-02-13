552cf31e25a9a9e9788e5dfeb6347a30194f7d3d
Ronald Ekambi <ronald.ekambi@cenx.com>
2016-02-13 09:11:32 -0500

    added logo and html

--- organator/www/js/app.js
+++ organator/www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','ui.router','starter.controllers'])
+angular.module('starter', ['ionic','ui.router','starter.controllers', 'ngCordova'])
 
-.run(function ($ionicPlatform) {
+.run(function ($ionicPlatform, $cordovaSQLite) {
 	$ionicPlatform.ready(function () {
 		if (window.cordova && window.cordova.plugins.Keyboard) {
 			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -20,6 +20,10 @@ angular.module('starter', ['ionic','ui.router','starter.controllers'])
 		if (window.StatusBar) {
 			StatusBar.styleDefault();
 		}
+        
+        
+        db = $cordovaSQLite.openDB("my.db");
+        $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
 	});
 })
 .config(function ($stateProvider, $urlRouterProvider) {
