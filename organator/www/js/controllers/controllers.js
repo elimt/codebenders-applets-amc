6250228c7f3582713cc23843449e813d1d1ef6de
Ronald Ekambi <ronald.ekambi@cenx.com>
2016-02-13 09:47:01 -0500

    added database to controllers

--- organator/www/js/controllers/controllers.js
+++ organator/www/js/controllers/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', [ 'ionic', 'ngCordova'])
 
 .controller('AppCntrl', function ($scope, $state) {
 
@@ -35,13 +35,34 @@ angular.module('starter.controllers', [])
 		$scope.init = function () {}
 
 })
-.controller('hospitalHomeCtrl', function($scope){
+.controller('hospitalHomeCtrl', function($scope, $cordovaSQLite){
     $scope.init = function(){}
     $scope.listOfDonors = [
             {name:'ron', country:'Norway'},
             {name:'Hege',country:'Sweden'},
             {name:'Kai',country:'Denmark'}
     ];
+    $scope.insert = function(firstname, lastname) {
+        var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+        $cordovaSQLite.execute(db, query, [firstname, lastname]).then(function(res) {
+            console.log("INSERT ID -> " + res.insertId);
+        }, function (err) {
+            console.error(err);
+        });
+    }
+ 
+    $scope.select = function(lastname) {
+        var query = "SELECT firstname, lastname FROM people WHERE lastname = ?";
+        $cordovaSQLite.execute(db, query, [lastname]).then(function(res) {
+            if(res.rows.length > 0) {
+                console.log("SELECTED -> " + res.rows.item(0).firstname + " " + res.rows.item(0).lastname);
+            } else {
+                console.log("No results found");
+            }
+        }, function (err) {
+            console.error(err);
+        });
+    }
    
 })
 .controller('whyToDonateCtrl', function ($scope) {
